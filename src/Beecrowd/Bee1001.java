32bf1c17fc9e5080a09e22102fb159ea62b0db65
Isadora-Kairala <isadorarequer7191@gmail.com>
2026-02-19 20:47:05 -0300

    Reorganizando exercícios Beecrowd em Java

---
+++ src/Beecrowd/Bee1001.java
@@ -0,0 +1,14 @@
+package Beecrowd;
+import java.util.Scanner;
+
+
+public class Bee1001 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Seu código aqui
+
+        sc.close();
+    }
+}
